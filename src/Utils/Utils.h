57b3e811538089bcda07f51ae2b6c9aa99ee4a24
CXUtk <2237367067@qq.com>
2021-08-29 17:04:05 +0800

    放弃C++20

--- src/Utils/Utils.h
+++ src/Utils/Utils.h
@@ -1,10 +1,14 @@
-﻿#include <string>
+﻿#pragma once
+#include <exception>
+#include <cstdio>
+#include <memory>
+#include <string>
 
 template<typename ... Args>
 std::string string_format(const std::string& format, Args ... args)
 {
     int size_s = std::snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
-    if (size_s <= 0) { throw std::runtime_error("Error during formatting."); }
+    if (size_s <= 0) { throw std::exception("Error during formatting."); }
     auto size = static_cast<size_t>(size_s);
     auto buf = std::make_unique<char[]>(size);
     std::snprintf(buf.get(), size, format.c_str(), args ...);
